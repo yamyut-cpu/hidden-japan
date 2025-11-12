e6a4160ba173a474c32513eb1e66a1a368231b37
yamyut-cpu <yamyut1515@gmail.com>
2025-11-12 15:51:29 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.jsx";  // ← ここが .jsx になっていること！
 
 createRoot(document.getElementById("root")).render(<App />);
